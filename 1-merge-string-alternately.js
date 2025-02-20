b2dcaef16eb98064886fd5a7ceff5ffd8c620846
Akil Tajwar <akiltajwar@gmail.com>
2025-02-20 11:13:06 +0600

    1-merge-string-alternately

--- 1-merge-string-alternately.js
+++ 1-merge-string-alternately.js
@@ -1,4 +1,33 @@
-const word1 = "abc";
+const word1 = "abcdefg";
 const word2 = "pqr";
-let output = ''
+let output = ""
 
+for(let i = 0; i < word1.length || i < word2.length; i++) {
+    if(i < word1.length) {
+        output += word1[i];
+    }
+    if(i < word2.length) {
+        output += word2[i];
+    }
+}
+console.log(output)
+
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Enter the first word: ', (word1) => {
+//     rl.question('Enter the second word: ', (word2) => {
+//         let output = "";
+//         for(let i = 0; i < word1.length || i < word2.length; i++) {
+//             if(i < word1.length) output += word1[i];
+//             if(i < word2.length) output += word2[i];
+//         }
+//         console.log(output);
+//         rl.close();
+//     });
+// });
